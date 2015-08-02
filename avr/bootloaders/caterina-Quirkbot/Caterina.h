1698a842948ecfa5ae0efa74fa66e530decfdf85
Paulo Barcelos <paulo@azucrina.org>
2015-08-02 13:41:38 +0300

    Increased bootaloder time to 20 seconds

--- avr/bootloaders/caterina-Quirkbot/Caterina.h
+++ avr/bootloaders/caterina-Quirkbot/Caterina.h
@@ -63,16 +63,16 @@
 
 		/** Eight character bootloader firmware identifier reported to the host when requested */
 		#define SOFTWARE_IDENTIFIER          "QUIRKBOT"
-		
+
 		#define CPU_PRESCALE(n)	(CLKPR = 0x80, CLKPR = (n))
 		#define LED_SETUP()		DDRC |= (1<<7); DDRB |= (1<<0); DDRD |= (1<<5);
 		#define L_LED_OFF()		PORTC &= ~(1<<7)
 		#define L_LED_ON()		PORTC |= (1<<7)
 		#define L_LED_TOGGLE()	PORTC ^= (1<<7)
-		#define TX_LED_OFF()	PORTD |= (1<<5)
-		#define TX_LED_ON()		PORTD &= ~(1<<5)
-		#define RX_LED_OFF()	PORTB |= (1<<0)
-		#define RX_LED_ON()		PORTB &= ~(1<<0)
+		#define TX_LED_OFF()	PORTD &= ~(1<<5)
+		#define TX_LED_ON()		PORTD |= (1<<5)
+		#define RX_LED_OFF()	PORTB &= ~(1<<0)
+		#define RX_LED_ON()		PORTB |= (1<<0)
 
 	/* Type Defines: */
 		/** Type define for a non-returning pointer to the start of the loaded application in flash memory. */
@@ -81,7 +81,7 @@
 	/* Function Prototypes: */
 		void StartSketch(void);
 		void LEDPulse(void);
-	
+
 		void CDC_Task(void);
 		void SetupHardware(void);
 
@@ -96,4 +96,3 @@
 		#endif
 
 #endif
-
