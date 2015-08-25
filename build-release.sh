740c35fbabe92db874970c9054f569949ded1a4b
Paulo Barcelos <paulo@azucrina.org>
2015-08-25 07:20:24 +0200

    / new way of building release

--- build-release.sh
+++ build-release.sh
@@ -1,15 +1,22 @@
+
+rm -r quirkbot-avr
+
+cp -r avr quirkbot-avr
+cp quirkbot-avr/bootloaders/caterina-Quirkbot/Caterina-Quirkbot.hex __Caterina-Quirkbot.hex
+rm -r quirkbot-avr/bootloaders/caterina-Quirkbot/*
+mv __Caterina-Quirkbot.hex quirkbot-avr/bootloaders/caterina-Quirkbot/Caterina-Quirkbot.hex
+rm -r quirkbot-avr.tar.bz2
+tar -jcf quirkbot-avr.tar.bz2 quirkbot-avr
+
 rm QuirkbotArduinoHardware.zip
 zip -vr QuirkbotArduinoHardware.zip \
-avr \
+quirkbot-avr \
 -x "*.DS_Store"
+
+rm -r quirkbot-avr
 
 clear
 
-cp -r avr quirkbot-avr
-rm -r quirkbot-avr/bootloaders/caterina-Quirkbot/LUFA-111009
-rm -r quirkbot-avr.tar.bz2
-tar -jcf quirkbot-avr.tar.bz2 quirkbot-avr
-rm -r quirkbot-avr
 
 echo
 echo
