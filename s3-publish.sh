859d9ec34787d57accb864c04c351eac7d8055e3
Paulo Barcelos <paulo@azucrina.org>
2015-10-01 15:53:49 +0200

    Updated arduino core

--- s3-publish.sh
+++ s3-publish.sh
@@ -1,7 +1,7 @@
 rm -r s3_publish
 mkdir s3_publish
 
-cp quirkbot-avr.tar.bz2 s3_publish/quirkbot-avr.tar.bz2
+#cp quirkbot-avr.tar.bz2 s3_publish/quirkbot-avr.tar.bz2
 cp QuirkbotArduinoHardware.zip s3_publish/QuirkbotArduinoHardware.zip
 
 aws s3 sync s3_publish s3://code.quirkbot.com/downloads/ --exclude *.DS_Store
