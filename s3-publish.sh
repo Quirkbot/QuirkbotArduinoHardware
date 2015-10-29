a0dc8f3f3a3e4150709268ec96e452c9d5c58d33
Paulo Barcelos <paulo@azucrina.org>
2015-10-29 16:42:08 +0100

    Updated publish script

--- s3-publish.sh
+++ s3-publish.sh
@@ -1,10 +1,13 @@
+sh "build-release.sh"
+
 rm -r s3_publish
 mkdir s3_publish
 
-#cp quirkbot-avr.tar.bz2 s3_publish/quirkbot-avr.tar.bz2
-cp QuirkbotArduinoHardware.zip s3_publish/QuirkbotArduinoHardware.zip
-
+cp "QuirkbotArduinoHardware.zip" "s3_publish/quirkbot-arduino-hardware.zip"
 aws s3 sync s3_publish s3://code.quirkbot.com/downloads/ --exclude *.DS_Store
 
-
 rm -r s3_publish
+mkdir s3_publish
+
+cp "QuirkbotArduinoHardware.zip" "s3_publish/QuirkbotArduinoHardware.zip"
+aws s3 sync s3_publish s3://code.quirkbot.com/downloads/ --exclude *.DS_Store
