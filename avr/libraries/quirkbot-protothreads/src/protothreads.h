8df61e14f4ee71403352098e454aa660a0101b22
Paulo Barcelos <paulo@azucrina.org>
2020-09-18 14:08:14 +0200

    Updated protothreads interface to better handle threads with arguments.

--- avr/libraries/quirkbot-protothreads/src/protothreads.h
+++ avr/libraries/quirkbot-protothreads/src/protothreads.h
@@ -3,25 +3,25 @@
 
 /**
 * ADDED BY STRAWBEES
-* Provide a simplied interface to Protothreads, avoid the need to repeat
+* Provides a simplied interface to Protothreads.
 */
 #define PT_POINTER_VAR(name) pt_##name##_pointer
 #define PT_THREAD_VAR(name) pt_##name##_thread
 
-#define ptDefine(name)\
+#define ptDefine(name, ...)\
 	static struct pt PT_POINTER_VAR(name);\
-	static PT_THREAD (PT_THREAD_VAR(name)(struct pt* __pt__));
-#define ptDeclare(name) static PT_THREAD (PT_THREAD_VAR(name)(struct pt* __pt__))
+	static PT_THREAD (PT_THREAD_VAR(name)(struct pt* __pt__, ##__VA_ARGS__));
+#define ptDeclare(name, ...) static PT_THREAD (PT_THREAD_VAR(name)(struct pt* __pt__, ##__VA_ARGS__))
 #define ptInit(name) PT_INIT(&PT_POINTER_VAR(name))
 #define ptBegin() PT_BEGIN(__pt__)
 #define ptSleep(ms) PT_SLEEP(__pt__, ms)
 #define ptWaitUntil(condition) PT_WAIT_UNTIL(__pt__, condition)
 #define ptWaitWhile(condition) PT_WAIT_WHILE(__pt__, condition)
-#define ptWaitThread(name) PT_WAIT_THREAD(__pt__, PT_THREAD_VAR(name)(&PT_POINTER_VAR(name)))
+#define ptWaitThread(name, ...) PT_WAIT_THREAD(__pt__, PT_THREAD_VAR(name)(&PT_POINTER_VAR(name), ##__VA_ARGS__))
 #define ptYield() PT_YIELD(__pt__)
 #define ptYieldUntil(condition) PT_YIELD_UNTIL(__pt__, condition)
-#define ptSpawn(name) PT_SPAWN(__pt__, &PT_POINTER_VAR(name), PT_THREAD_VAR(name)(&PT_POINTER_VAR(name)))
+#define ptSpawn(name, ...) PT_SPAWN(__pt__, &PT_POINTER_VAR(name), PT_THREAD_VAR(name)(&PT_POINTER_VAR(name), ##__VA_ARGS__))
 #define ptRestart() PT_RESTART(__pt__)
 #define ptExit() PT_EXIT(__pt__)
 #define ptEnd() PT_END(__pt__)
-#define ptSchedule(name) PT_SCHEDULE(PT_THREAD_VAR(name)(&PT_POINTER_VAR(name)))
+#define ptSchedule(name, ...) PT_SCHEDULE(PT_THREAD_VAR(name)(&PT_POINTER_VAR(name), ##__VA_ARGS__))
